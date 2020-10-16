2591031bccc3ae0c8c27ea40efd544d1e2086c1e
DanielMeireles <daniel.meireles@outlook.com>
2020-10-15 22:37:29 -0300

    Add cards of Shopping List on Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import { FiPower } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Container, Header, HeaderContent, HeaderProfile } from './styles';
+import { format } from 'date-fns';
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  HeaderProfile,
+  ShoppingListsTitle,
+  ShoppingLists,
+  ShoppingListContainer,
+  ShoppingListImage,
+  ShoppingListInfo,
+  ShoppingListName,
+  Calendar,
+  Info,
+  ShoppingListMeta,
+  ShoppingListMetaText,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+export interface ShoppingList {
+  id: string;
+  name: string;
+  description: string;
+  date: Date;
+  user_id: string;
+  image_url: string;
+}
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
+
+  const [shoppingLists, setShoppingLists] = useState<ShoppingList[]>([]);
+
+  const handleGetShoppingLists = useCallback(() => {
+    api.get('/shoppinglists').then((response) => {
+      const shoppingListsResponse: ShoppingList[] = response.data;
+      setShoppingLists(
+        shoppingListsResponse.sort((a, b) => {
+          if (a.date > b.date) {
+            return 1;
+          }
+          return -1;
+        }),
+      );
+    });
+  }, []);
+
+  useEffect(() => {
+    handleGetShoppingLists();
+  }, []);
 
   return (
     <Container>
@@ -30,6 +77,29 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <ShoppingLists>
+        <ShoppingListsTitle>Listas de Compras</ShoppingListsTitle>
+        {shoppingLists.map((shoppingList) => (
+          <ShoppingListContainer>
+            <ShoppingListImage src={shoppingList.image_url} />
+            <ShoppingListInfo>
+              <ShoppingListName>{shoppingList.name}</ShoppingListName>
+              <ShoppingListMeta>
+                <Calendar size={20} />
+                <ShoppingListMetaText>
+                  {format(new Date(shoppingList.date), 'dd/MM/yyyy')}
+                </ShoppingListMetaText>
+              </ShoppingListMeta>
+              <ShoppingListMeta>
+                <Info size={20} />
+                <ShoppingListMetaText>
+                  {shoppingList.description}
+                </ShoppingListMetaText>
+              </ShoppingListMeta>
+            </ShoppingListInfo>
+          </ShoppingListContainer>
+        ))}
+      </ShoppingLists>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
+import { FiCalendar, FiInfo } from 'react-icons/fi';
 
 export const Container = styled.div`
   font-family: 'Poppins';
 `;
 
 export const Header = styled.header`
+  position: sticky;
+  top: 0;
   padding: 32px 0;
   background: ${({ theme }) => theme.colors.headerBackground};
 `;
@@ -61,4 +64,67 @@ export const HeaderProfile = styled.div`
       }
     }
   }
+`;
+
+export const ShoppingListsTitle = styled.h1`
+  font-size: 24px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.colors.primaryText};
+`;
+
+export const ShoppingLists = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 15px;
+`;
+
+export const ShoppingListContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  background: ${({ theme }) => theme.colors.cardBackground};
+  border-radius: 10px;
+  padding: 20px;
+  align-items: center;
+  width: 400px;
+  margin-bottom: 15px;
+`;
+
+export const ShoppingListImage = styled.img`
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+`;
+
+export const ShoppingListInfo = styled.div`
+  flex: 1;
+  margin-left: 24px;
+`;
+
+export const ShoppingListName = styled.h1`
+  color: ${({ theme }) => theme.colors.cardTitle};
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
+
+export const Calendar = styled(FiCalendar)`
+  color: ${({ theme }) => theme.colors.cardElement};
+`;
+
+export const Info = styled(FiInfo)`
+  color: ${({ theme }) => theme.colors.cardElement};
+`;
+
+export const ShoppingListMeta = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 5px;
+`;
+
+export const ShoppingListMetaText = styled.span`
+  margin-left: 8px;
+  margin-top: 4px;
+  color: ${({ theme }) => theme.colors.cardText};
 `;
